f2534ea40a823aa1200466f567b3b335e440fcb6
Wojtek <kozaneckiwojtek@gmail.com>
2023-07-17 21:41:50 +0200

    changes

--- src/sites/baner.js
+++ src/sites/baner.js
@@ -7,6 +7,7 @@ import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 
 import Symbol from "../../src/img/symbol.png";
+import Logo from "../../src/img/logo_transparent.png";
 import "../../src/css/style_baner.css";
 
 
@@ -22,17 +23,17 @@ const Baner = () => {
         <>
             <Container fluid className='baner'>
                 <Row>
-                    <Col className='logo col-6 col-sm-6 col-md-6 col-lg-2 col-xl-2 d-flex align-items-center justify-content-center'>
+                    <Col className='logo col-6 col-sm-6 col-md-6 col-lg-2 col-xl-2 col-xxl-2 d-flex align-items-center justify-content-center'>
                         <a id="logotop" href="./">
                             <img src={Symbol} alt="logo" className="logo-image" style={{ width: "150px"}}/>
                         </a>
                     </Col>
 
-                    <Col id='adres_kontener' className='logo col-6 col-sm-6 col-md-6 col-lg-2 col-xl-2 d-flex align-items-center justify-content-begin'>
+                    <Col id='adres_kontener' className='logo col-6 col-sm-6 col-md-6 col-lg-2 col-xl-2 col-xl-2 d-flex align-items-center justify-content-begin'>
                         <p id='adres'>83-200, Starogard Gdański <br/> ul. Lubichowska 91 <br/> Tel. 58 56 11 888</p>
                     </Col>
 
-                    <Col className='logo col-12 col-sm-12 col-md-12 col-lg-8 col-xl-8 d-flex align-items-center justify-content-center'>
+                    <Col className='logo col-12 col-sm-12 col-md-12 col-lg-8 col-xl-8 col-xxl-5 d-flex align-items-center justify-content-center'>
                         <Navbar expand="lg" expanded={expanded} onToggle={handleNavToggle}>
                             <Navbar.Toggle aria-controls="basic-navbar-nav" className="custom-hamburger" />
                             <Navbar.Collapse id="basic-navbar-nav">
@@ -46,6 +47,9 @@ const Baner = () => {
                                 </Nav>
                             </Navbar.Collapse>
                         </Navbar>
+                    </Col>
+                    <Col className='logo col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-3 d-flex align-items-center justify-content-center'>
+                        <img src={Logo}/>
                     </Col>
                 </Row>
 

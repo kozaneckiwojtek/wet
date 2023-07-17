f8d89557d55a225e4ba7a7aefec538964f4be3fd
Wojtek <kozaneckiwojtek@gmail.com>
2023-07-17 20:06:49 +0200

    changes

--- src/sites/baner.js
+++ src/sites/baner.js
@@ -37,11 +37,11 @@ const Baner = () => {
                             <Navbar.Toggle aria-controls="basic-navbar-nav" className="custom-hamburger" />
                             <Navbar.Collapse id="basic-navbar-nav">
                                 <Nav className="mr-auto">
-                                    <Nav.Link className='baner_elemen' href="/">Witamy</Nav.Link>
-                                    <Nav.Link className='baner_elemen' href="/oferta">Oferta</Nav.Link>
-                                    <Nav.Link className='baner_elemen' href="/galeria">Galeria</Nav.Link>
-                                    <Nav.Link className='baner_elemen' href="/lokalizacja">Lokalizacja</Nav.Link>
-                                    <Nav.Link className='baner_elemen' href="/kontakt">Kontakt</Nav.Link>
+                                    <Nav.Link className='baner_elemen' href="/"><Link className='nav_element' to="/">Witamy</Link></Nav.Link>
+                                    <Nav.Link className='baner_elemen' href="/oferta"><Link className='nav_element' to="/oferta">Oferta</Link></Nav.Link>
+                                    <Nav.Link className='baner_elemen' href="/galeria"><Link className='nav_element' to="/galeria">Galeria</Link></Nav.Link>
+                                    <Nav.Link className='baner_elemen' href="/lokalizacja"><Link className='nav_element' to="/lokalizacja">Lokalizacja</Link></Nav.Link>
+                                    <Nav.Link className='baner_elemen' href="/kontakt"><Link className='nav_element' to="/kontakt">Kontakt</Link></Nav.Link>
 
                                 </Nav>
                             </Navbar.Collapse>

3c39cf4568a7900eba9e1d917cee185304ee365c
Wojtek <kozaneckiwojtek@gmail.com>
2023-07-20 17:20:39 +0200

    changes

--- src/sites/kontakt.js
+++ src/sites/kontakt.js
@@ -8,7 +8,7 @@ export function Kontakt() {
     return (
         <Container className='kontakt'>
             <Row>
-                <Col md={6} className="contact-info">
+                <Col className="contact-info">
                     <h2>PRZYCHODNIA WETERYNARYJNA</h2>
                     <p>Zbigniew Wojewódzki</p>
                     <p>ul. Lubichowska 91</p>
@@ -20,12 +20,7 @@ export function Kontakt() {
                     <p id='telefon'><strong>Telefon: 58 56 11 888</strong></p>
                     </div>
                 </Col>
-                <Col md={6} className="contact-info">
-                    <h3>Telefony kontaktowe:</h3>
-                    <p>Lek. wet. Zbigniew Wojewódzki: 501 710 101</p>
-                    <p>Lek. wet. Magdalena Grabowska: 666 555 120</p>
-                    <p>Lek. wet. Michał Pajkowski: 503 808 108</p>
-                </Col>
+            
             </Row>
         </Container>
     );
